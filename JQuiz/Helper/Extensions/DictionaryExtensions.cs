83f83a79327b80f887e7c1bd81d15b1613875fad
Amer Shehady <shehadyamer@gmail.com>
2021-07-31 10:37:03 +0300

    Added a way to check whether the same question has been added twice and remove it.

--- JQuiz/Helper/Extensions/DictionaryExtensions.cs
+++ JQuiz/Helper/Extensions/DictionaryExtensions.cs
@@ -12,6 +12,11 @@ namespace JQuiz.Extensions
         {
             for (int x = 0; x <= splittedContent.Length - 1; x++)
             {
+                if (dic.ContainsKey(splittedContent[x]))
+                {
+                    x++;
+                    continue;
+                }
                 dic.Add(splittedContent[x], splittedContent[++x]);
             }
         }
